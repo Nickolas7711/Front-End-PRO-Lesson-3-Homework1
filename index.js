1753f9cb9ebc2b552987d3a50986b415a45dc8c6
Mykola Kuniev <Nickolas7711@gmail.com>
2023-04-27 12:25:56 +0300

    Lesson 3/ homework 1

---
+++ index.js
@@ -0,0 +1,5 @@
+queryFirstValue = +prompt('Enter the first number to calculate the arithmetic mean');
+querySecondValue = +prompt('Enter the second number to calculate the arithmetic mean');
+queryThirdValue = +prompt('Enter the third number to calculate the arithmetic mean');
+calculationArithmeticMean = (queryFirstValue + querySecondValue + queryThirdValue) / 3;
+alert(`Arithmetic mean value of given numbers = ${calculationArithmeticMean}`);
